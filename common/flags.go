d65059737a3388aaed2a1e8605376b03b01378c2
Vitor Falcao <vitorfhcosta@gmail.com>
2019-11-17 16:53:20 -0300

    Adds pin flag

--- common/flags.go
+++ common/flags.go
@@ -2,20 +2,29 @@ package common
 
 import (
 	"flag"
+	"log"
 	"strings"
 )
 
-// JSONPath is the variable holding the filesystem
-// path to the cfg.json
-var JSONPath string
+var (
+	// JSONPath is the variable holding the filesystem path to the cfg.json
+	JSONPath string
+	// Pin is the variable holding the controller's pin code
+	Pin string
+)
 
 // SetFlags sets the command line interface flags
 func SetFlags() {
 	flag.StringVar(&JSONPath, "cfg", "./cfg.json", "Path to the cfg.json file")
+	flag.StringVar(&Pin, "pin", "", "Pin code for the controller")
 	flag.Parse()
 	fixFlags()
 }
 
 func fixFlags() {
 	JSONPath = strings.Trim(JSONPath, " ")
+	Pin = strings.Trim(Pin, " ")
+	if Pin == "" {
+		log.Fatal("Pin not provided through -pin flag")
+	}
 }
