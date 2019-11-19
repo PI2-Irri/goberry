cfb040a20beaca1f278790fa4855334d5825610b
Vitor Falcao <vitorfhcosta@gmail.com>
2019-11-19 17:21:43 -0300

    Concurrent sendings

--- measurement/queue.go
+++ measurement/queue.go
@@ -1,15 +1,23 @@
 package measurement
 
+import "log"
+
 // Queue is a channel which receives all Senders which
 // need to run the Send() function
 var Queue chan Sender
 
+const threads = 4
+
 func init() {
 	Queue = make(chan Sender, 1024)
-	go consume()
+	log.Println("Starting", threads, "consumers")
+	for i := 0; i < threads; i++ {
+		go consume()
+	}
 }
 
 func consume() {
+	log.Println("Consumer up")
 	for sndr := range Queue {
 		sndr.Send()
 	}
